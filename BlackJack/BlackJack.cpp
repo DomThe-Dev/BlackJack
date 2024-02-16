1d527f9b6892b24fbeb08f62f493105e5c893ad4
DomThe Dev <dominykas.jucys1@gmail.com>
2024-02-16 17:25:26 +0000

    Add project files.

---
+++ BlackJack/BlackJack.cpp
@@ -0,0 +1,15 @@
+/*
+BlackJack V1.1
+By Dominykas Jucys
+
+The previous GitHub repository has been deleted, due to it being made when I knew little about GitHub
+meaning that the file structure and such was an absolute mess. Nonetheless, the code from there will be 
+copied over here, in the hopes of showing off the very first version of my BlackJack game.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
